599d8c917aaf044cd2425ed52232759c0eb4440c
Michał Śnieżko <michalsniezko@gmail.com>
2025-07-29 18:19:47 +0200

    router with attributes

--- app/Services/EmailService.php
+++ app/Services/EmailService.php
@@ -6,10 +6,9 @@ namespace App\Services;
 
 class EmailService
 {
-    public function send(array $to, string $template): bool
+    public function send(array $to, string $template): void
     {
 //        sleep(1);
 
-        return true;
     }
 }
